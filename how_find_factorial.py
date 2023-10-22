0ed276892b4628d67b42934416daae9e5b31ab3e
one mriganka <mribarman215@gmail.com>
2023-10-22 16:36:04 +0530

    added to find factorial code

---
+++ how_find_factorial.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers"
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+# Example usage:
+n = 5
+print(f"The factorial of {n} is {factorial(n)}")
