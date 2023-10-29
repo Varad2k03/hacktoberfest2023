79ff6537783eafd95bc2cb3492a9d1a2f977e796
kdake40 <115654959+kdake40@users.noreply.github.com>
2023-10-29 22:57:45 +0530

    Create squareroot.py

---
+++ squareroot.py
@@ -0,0 +1,12 @@
+# Square root program in Python
+
+import math
+
+# Take input from the user
+num = float(input("Enter a number: "))
+
+# Calculate the square root
+sqrt = math.sqrt(num)
+
+# Print the result
+print("The square root of", num, "is", sqrt)
