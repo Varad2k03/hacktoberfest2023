57e48eb18a9c7f52a48d2e5b470dec37ab35d382
kdake40 <115654959+kdake40@users.noreply.github.com>
2023-10-29 22:55:48 +0530

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,12 @@
+
+str_1 = input (“Enter a string” “)
+
+words = [word.lower () for word in str_1.split ()]
+
+words.sort ()
+
+print (“The words sorted in alphabetical order are as follows: “)
+
+ for word in words:
+
+              print (word)
